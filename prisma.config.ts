573db8102cb4c1441af8fe0e3ca1c5a569278420
victor-hugo-ss <victorabreu2512@gmail.com>
2026-07-09 13:47:48 -0300

    fix: corrige build docker na railway

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,12 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
-const databaseUrl =
-  env("DATABASE_URL") ?? "postgresql://user:password@localhost:5432/card_debts";
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: databaseUrl,
+    url: env("DATABASE_URL"),
   },
 });
